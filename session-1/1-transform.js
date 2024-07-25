46dac6541f12713f0b92ef39414cadbfce71d8e3
Zeraphim <diamantejc87@gmail.com>
2024-07-26 00:13:33 +0800

    1-transform.js, added exception handling

--- session-1/1-transform.js
+++ session-1/1-transform.js
@@ -5,26 +5,37 @@ Write a function named transform that accepts an array of integers, and returns 
 */
 
 function transform(nums) {
-  // An array variable to store the squared numbers
-  let squaredNums = []
+    try {
+        // If nums is not an array then return an error
+        if (!Array.isArray(nums)) {
+            throw new TypeError('Input must be an array');
+        }
 
-  // Loop through each element in the input array
-  for (let iterator = 0; iterator < nums.length; iterator++) {
-    const squaredNum = nums[iterator] * nums[iterator]
+        // An array variable to store the squared numbers
+        let squaredNums = []
 
-    // Squaring the element and push it to the new array
-    squaredNums.push(squaredNum)
-  }
+        // Loop through each element in the input array
+        for (let iterator = 0; iterator < nums.length; iterator++) {
+            const squaredNum = nums[iterator] * nums[iterator]
 
-  // Sorting the new array in ascending order
-  squaredNums.sort(function(a, b) {
-    // If the function returns a negative value, place a before b.
-    // If the fucntion returns zero, the order of a and b will remain unchanged.
-    // If the function returns a positive value, a will be placed after b.
-    return a - b
-  })
+            // Squaring the element and push it to the new array
+            squaredNums.push(squaredNum)
+        }
 
-  return squaredNums;
+        // Sorting the new array in ascending order
+        squaredNums.sort(function(a, b) {
+            // If the function returns a negative value, place a before b.
+            // If the fucntion returns zero, the order of a and b will remain unchanged.
+            // If the function returns a positive value, a will be placed after b.
+            return a - b
+        })
+
+        return squaredNums;
+
+    } catch (error) {
+        // If there's an error, log the error message to the console
+        console.error('Error:', error.message);
+    }
 }
 
 const nums = [4,9,5,3,8]
