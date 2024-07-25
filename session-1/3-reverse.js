b70a10cffd9dcc6fe51a76c274153f44363510e3
Zeraphim <diamantejc87@gmail.com>
2024-07-25 23:53:35 +0800

    3-reverse solved

--- session-1/3-reverse.js
+++ session-1/3-reverse.js
@@ -1,11 +1,20 @@
 /*
 
-Write a function named reverse that, when given an input string, returns the reversed version of that string. Use traditional for loop. Name this file 3-reverse.js.
+Write a function named reverse that, when given an input string, returns the reversed version of that string. Use traditional for loop. Name this file
 
 */
 
 function reverse(word) {
- // ... some logic here
+    // String var to place all of the reversed characters
+    let reversed = '';
+
+    // Loop through the word from the last character to the first character
+    for (let iterator = word.length - 1; iterator >= 0; iterator--) {
+        // Concatenate the characters to the reversed variable
+        reversed += word[iterator];
+    }
+    
+    return reversed;
 }
 
 const word = 'hello'
