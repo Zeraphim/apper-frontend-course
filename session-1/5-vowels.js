51033a8f07ec4718f99dc5ff357af373ada4f87a
Zeraphim <diamantejc87@gmail.com>
2024-07-26 00:02:38 +0800

    5-vowels solved

--- session-1/5-vowels.js
+++ session-1/5-vowels.js
@@ -5,7 +5,30 @@ Write a function that returns only vowels with a given input string. Name this f
 */
 
 function convertToVowels(word) {
-  // ... some logic here
+    try {
+        // If word is not a string then throw an error
+        if (typeof word !== 'string') {
+            throw new TypeError('Input must be a string');
+        }
+
+        // Match vowels in the input string, searches through the word for matches in the regex. [Global flag and Case-Insensitive Flag]
+        const vowelsArray = word.match(/[aeiou]/gi);
+
+        // Check if the result is not null
+        if (vowelsArray) {
+            // Join array of vowels into a string
+            return vowelsArray.join('');
+        } else {
+            // Return empty string if no vowels are found
+            return '';
+        }
+
+    } catch (error) {
+        // If there's an error show the error in the terminal
+        console.error('Error:', error.message);
+        return '';
+
+    }
 }
 
 const word = 'thisissomeword'
