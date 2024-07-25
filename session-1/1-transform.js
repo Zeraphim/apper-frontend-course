d90910456a115eaeff0ffd59fbf3bab31737680a
Zeraphim <diamantejc87@gmail.com>
2024-07-25 23:40:08 +0800

    1-transform solved

--- session-1/1-transform.js
+++ session-1/1-transform.js
@@ -1,11 +1,30 @@
 /*
 
-Write a function named transform that accepts an array of integers, and returns an array in which all the elements are squared and sorted in ascending order. Name this file 1-transform.js.
+Write a function named transform that accepts an array of integers, and returns an array in which all the elements are squared and sorted in ascending order.
 
 */
 
 function transform(nums) {
-  // ...some logic here
+  // An array variable to store the squared numbers
+  let squaredNums = []
+
+  // Loop through each element in the input array
+  for (let iterator = 0; iterator < nums.length; iterator++) {
+    const squaredNum = nums[iterator] * nums[iterator]
+
+    // Squaring the element and push it to the new array
+    squaredNums.push(squaredNum)
+  }
+
+  // Sorting the new array in ascending order
+  squaredNums.sort(function(a, b) {
+    // If the function returns a negative value, place a before b.
+    // If the fucntion returns zero, the order of a and b will remain unchanged.
+    // If the function returns a positive value, a will be placed after b.
+    return a - b
+  })
+
+  return squaredNums;
 }
 
 const nums = [4,9,5,3,8]
